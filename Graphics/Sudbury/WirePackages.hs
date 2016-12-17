a20bfd18b1d8db13f9779f667ecf8e6e0c6095ca
Dorus Leliveld <dorusleliveld@gmail.com>
2016-12-17 17:53:38 -0600

    Fixed list decoding and encoding The list instances used previously were incorrect as the dont account for padding and do not have the correct format (prepend the size of the list)

--- Graphics/Sudbury/WirePackages.hs
+++ Graphics/Sudbury/WirePackages.hs
@@ -11,10 +11,15 @@ module Graphics.Sudbury.WirePackages where
 
 import Data.Word
 import Data.Store
+import Data.Foldable
 import Data.Store.Core
+import Data.Store.Internal
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as B
 import qualified Data.ByteString.Builder as BB
+import Control.Monad
+import Foreign.Ptr (minusPtr)
+
 
 
 data WirePackage = WirePackage
@@ -43,14 +48,45 @@ instance Store WirePackage where
     plraw <- peekToPlainForeignPtr "Data.ByteString.ByteString" payloadSize
     let pl = B.PS plraw 0 payloadSize
     return $ WirePackage 
-      { wirePackageSender=sen
-      , wirePackageSize=sz 
-      , wirePackageOpcode=op 
-      , wirePackagePayload=pl
+      { wirePackageSender  = sen
+      , wirePackageSize    = sz
+      , wirePackageOpcode  = op
+      , wirePackagePayload = pl
       }
   {-# INLINE size #-}
   {-# INLINE peek #-}
   {-# INLINE poke #-}
+
+newtype WirePackageStream = WirePackageStream 
+  { unWirePackageStream :: [WirePackage]
+  } deriving (Eq, Show)
+
+instance Store WirePackageStream where
+  size = VarSize $ foldl' sumAlignedSize 0 . unWirePackageStream
+    where
+      VarSize f = size :: Size WirePackage
+      sumAlignedSize :: Int -> WirePackage -> Int
+      sumAlignedSize acc x = let pkgSize = f x in pkgSize + acc + (pkgSize `mod` 4)
+
+  poke (WirePackageStream xs) = traverse_ pokeAlign xs
+    where
+      pokeAlign wp = let align = fromIntegral $ wirePackageSize wp `mod` 4
+                         emptyByte = 0 :: Word8
+                      in poke wp >> replicateM align (poke emptyByte)
+
+  peek = WirePackageStream <$> go
+    where  
+      go :: Peek [WirePackage]
+      go = 
+        do rem <- remaining
+           if rem < 8 then Peek $ \_ ptr -> return (ptr, [])
+                      else do wp <- peek :: Peek WirePackage
+                              let align = fromIntegral $ wirePackageSize wp `mod` 4
+                              skip align
+                              (wp :) <$> go
+
+remaining :: Peek Int
+remaining = Peek $ \ps ptr -> return (ptr, peekStateEndPtr ps `minusPtr` ptr)
 
 wirePack :: WirePackage -> BB.Builder
 wirePack = BB.byteString . encode
